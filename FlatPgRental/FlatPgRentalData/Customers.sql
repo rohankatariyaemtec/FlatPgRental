aac79ec8dce174d7254e72bbac776cba296df9c9
Aman26Pawar <Amanjpawar.1997@gmail.com>
2018-07-27 15:34:27 +0530

    add gitignore file

---
+++ FlatPgRental/FlatPgRentalData/Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[CustomerId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [CustomerName] VARCHAR(50) NOT NULL, 
+    [ContactNumber] DECIMAL(10) NOT NULL, 
+    [EmailId] VARCHAR(100) NULL
+)
